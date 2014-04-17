18b03fb3e0dd61ffb1b3158b7a42f92b42387876
Daniel Staudigel <dstaudigel@gmail.com>
2014-04-17 13:47:04 -0700

    Add OAuth fields to credentials table.

---
+++ db/migrate/20140417204533_add_uid_and_provider_fields_to_credentials.rb
@@ -0,0 +1,6 @@
+class AddUidAndProviderFieldsToCredentials < ActiveRecord::Migration
+  def change
+    add_column :credentials, :provider, :string
+    add_column :credentials, :uid, :string
+  end
+end
