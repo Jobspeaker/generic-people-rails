e87c7bee7043d5f85eb0f60495c36779c8dbf0ee
Daniel Staudigel <dstaudigel@gmail.com>
2014-04-17 22:02:51 -0700

    Whoopsie.  This belongs in here.

--- db/migrate/20131129214324_create_api_tokens.rb
+++ db/migrate/20131129214324_create_api_tokens.rb
@@ -2,6 +2,7 @@ class CreateApiTokens < ActiveRecord::Migration
   def change
     create_table :api_tokens do |t|
       t.references :person, index: true
+      t.references :credential, index: true
       t.string :token
 
       t.timestamps
