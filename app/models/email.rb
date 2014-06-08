88312fe849dae819262466dc10bf156618f3fcf2
Brian J. Fox <bfox@OpusLogica.com>
2014-06-07 21:51:43 -0700

    Normalize email address

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,11 +2,17 @@ class Email < ActiveRecord::Base
   belongs_to :label
   has_and_belongs_to_many :people
 
+  before_save :force_address_to_lowercase
+
   def admin_object_name
     self.address
   end
 
   def person
     self.people.first rescue nil
+  end
+
+  def force_address_to_lowercase
+    self.address = self.address.downcase if self.address.present?
   end
 end
