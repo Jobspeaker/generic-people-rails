0a06d638512ab44fe012e86c4ebbcb5cf3998eed
Daniel Staudigel <dstaudigel@gmail.com>
2014-07-24 20:22:55 -0700

    Update as per bfox's instructions.

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -72,7 +72,7 @@ class Credential < ActiveRecord::Base
     self.permissions.find_or_create_by_permission_label_id(PermissionLabel.get(name).id)
   end
   
-  def as_json(options=nil)
+  def as_json(options={})
     super(:only => [:id , :member_id , :created_at])
   end
 
