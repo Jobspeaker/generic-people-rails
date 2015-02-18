49bb28db3df85b8e57d7a10affb3de8051addbb6
krae <krae@opuslogica.com>
2015-02-18 12:25:05 -0800

    resend confirmation email

--- app/controllers/generic_people_rails/credentials_controller.rb
+++ app/controllers/generic_people_rails/credentials_controller.rb
@@ -5,31 +5,57 @@ module GenericPeopleRails
     
     # for changing password
     def resend_confirmation
-        Rails.logger.debug "sending confirmation"
+      @params = params
+      email = @params[:email]
+      if email.present?
+        get_valid_cred(email)
+        if @cred
+          @cred.update(uid: SecureRandom.uuid)
+          GprMailer.resend_confirmation(@cred.member, @cred).deliver if defined?(ActionMailer)     
+          flash.now[:notice] = "Your confirmation email has been resent." 
+        end
+      end
     end
+    
+    def confirm_account
+      email = params[:email]
+      token = params[:token]     
+      uid = params[:uid]
+      if email.present? && token.present?
+        useremail = Email.where(address: email).first
+        cred = Credential.find_by(email_id: useremail.id, uid: token) if useremail
+        if useremail && cred
+          @member = cred.member
+          if @member.id == uid.to_i
+            @member.update(status: 'confirmed')
+            GprMailer.confirmed(@member).deliver        
+            flash[:notice] = "Thank you, your account has been confirmed!"
+          else
+            flash[:alert] = "Valid account not found"
+          end
+        else
+          flash[:alert] = "Valid account not found"
+        end
+      else
+        flash[:alert] = "Valid account not found"
+      end
+      redirect_to main_app.root_url
+    end    
   
     def forgot_password
       @params = params
       email = @params[:email]
       
       if email.present?
-        useremail = Email.where(address: Email.canonicalize_address(email)).first
-        if useremail
-          creds = Credential.where(email_id: useremail.id)
-          if creds.empty?
-            @msg = "Valid account not found."
-          else
-            cred = creds.first
-            #if cred.member.deleted_at.nil?
-              cred.update(uid: SecureRandom.uuid)
-              GprMailer.reset_password(cred.member, cred).deliver if defined?(ActionMailer)        
-              flash.now[:notice] = "An email has been sent to you with instructions on resetting your password."
-            #else
-              #flash.now[:error] = "Your account has been cancelled. Please contact an administrator"
-            #end
-          end
-        else
-          flash.now[:error] = "Valid account not found"
+        get_valid_cred(email)
+        if @cred
+          #if cred.member.deleted_at.nil?
+            @cred.update(uid: SecureRandom.uuid)
+            GprMailer.reset_password(@cred.member, @cred).deliver if defined?(ActionMailer)        
+            flash.now[:notice] = "An email has been sent to you with instructions on resetting your password."
+          #else
+            #flash.now[:error] = "Your account has been cancelled. Please contact an administrator"
+          #end
         end
       end
     end
@@ -72,6 +98,22 @@ module GenericPeopleRails
         #for these actions, the user can't be logged in
         if is_logged_in
           redirect_to main_app.root_url
+        end
+      end
+      
+      def get_valid_cred(email)
+        @email = Email.where(address: email).first
+        if @email
+          creds = Credential.where(email_id: @email.id)
+          if creds.empty?
+            @msg = "Valid account not found."
+          else
+            @cred = creds.first
+            #if cred.member.deleted_at.nil?
+            
+          end
+        else  
+          @msg = "Valid account not found."
         end
       end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 GenericPeopleRails::Engine.routes.draw do
   match "generic_people_rails/forgot_password" => "credentials#forgot_password" , as: :forgot_password, via: [:get , :post]
   match "generic_people_rails/reset_password" => "credentials#reset_password" , as: :reset_password, via: [:get , :post]  
-  match "generic_people_rails/resend_confirmation" => "credentials#resend_confirmation" , as: :resend_confirmation, via: :get 
+  match "generic_people_rails/resend_confirmation" => "credentials#resend_confirmation" , as: :resend_confirmation, via: [:get, :post]   
+  match "generic_people_rails/confirm_account" => "credentials#confirm_account" , as: :confirm_account, via: :get
 end
 
 Rails.application.routes.draw do
   match "forgot_password" => "generic_people_rails/credentials#forgot_password" , as: :forgot_password, via: [:get , :post]
   match "reset_password" => "generic_people_rails/credentials#reset_password" , as: :reset_password, via: [:get , :post]
-  match "resend_confirmation" => "generic_people_rails/credentials#resend_confirmation" , as: :resend_confirmation, via: :get   
+  match "resend_confirmation" => "generic_people_rails/credentials#resend_confirmation" , as: :resend_confirmation, via: [:get, :post]  
+  match "confirm_account" => "generic_people_rails/credentials#confirm_account" , as: :confirm_account, via: :get   
 end
