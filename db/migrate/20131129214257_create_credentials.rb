5944e4d71ad6b38685ff3a1e3366a28fc5ebfda9
Brian J. Fox <bfox@OpusLogica.com>
2015-06-10 14:25:12 -0700

    Be compatible with utf8mb4 and foreign keys

--- db/migrate/20131129214257_create_credentials.rb
+++ db/migrate/20131129214257_create_credentials.rb
@@ -3,8 +3,8 @@ class CreateCredentials < ActiveRecord::Migration
     create_table :credentials do |t|
       t.references :member, index: true
       t.references :email, index: true
-      t.string :password
-      t.string :provider, index: true
+      t.string :password, limit: 32
+      t.string :provider, limit: 127, index: true
       t.string :uid, index: true, limit: 64
 
       t.timestamps
