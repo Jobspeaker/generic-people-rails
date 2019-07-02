46b446d177d98d609f139bbea2385b5ef38c038f
Brian J. Fox <bfox@opuslogica.com>
2019-07-02 04:30:42 -0700

    Put back that which you have stolen from me!

--- app/models/person.rb
+++ app/models/person.rb
@@ -228,8 +228,6 @@ class Person < ActiveRecord::Base
     self.birthdate.zodiac_sign if self.birthdate.present?
   end
   
-  ## this is so not a cool thing to do. 
-=begin
   def email=(eaddr)
     if eaddr.is_a?(Email)
       emails << eaddr unless emails.include?(eaddr)
@@ -242,7 +240,7 @@ class Person < ActiveRecord::Base
     emails << e unless emails.include?(e)
     e.save
   end
-=end
+
   def email_address=(eaddr)
     e = emails.first rescue nil
     e ||= Email.create(:label => Label.get("Work"))
