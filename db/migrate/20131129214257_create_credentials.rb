c45b41a279747ae7be6c3d311de43a3608bb03e1
Brian J. Fox <bfox@opuslogica.com>
2019-07-03 07:39:37 -0700

    Too modern for me

--- db/migrate/20131129214257_create_credentials.rb
+++ db/migrate/20131129214257_create_credentials.rb
@@ -4,6 +4,7 @@ class CreateCredentials < ActiveRecord::Migration[5.2]
       t.references :member, index: true
       t.references :email, index: true
       t.string :password, limit: 32
+      t.string :password_digest, limit: 32
       t.string :provider, limit: 127, index: true
       t.string :uid, index: true, limit: 64
       t.string :salt
